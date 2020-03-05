3237c625d3fa9cfbd81c7c10ed7a2c6e24372a29
CHOISEUNGYOUN <alchemist718@gmail.com>
2020-03-05 21:58:23 +0900

    add function based login & logout

--- users/views.py
+++ users/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.views        import View
+from django.shortcuts    import render, redirect, reverse
+from django.contrib.auth import authenticate, login, logout
 
-# Create your views here.
+from . import forms
+
+class LoginView(View):
+    
+    def get(self, request):
+        form = forms.LoginForm(initial = {"email" : "youn@youn.com"})
+        return render(request, "users/login.html", context = { "form" : form })
+
+    def post(self, request):
+        form = forms.LoginForm(request.POST)
+        if form.is_valid():
+            email    = form.cleaned_data.get("email")
+            password = form.cleaned_data.get("password")
+            user = authenticate(request, username=email, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect(reverse("core:home"))
+
+        return render(request, "users/login.html", {"form": form})
+
+def log_out(request):
+    logout(request)
+    return redirect(reverse("core:home"))
