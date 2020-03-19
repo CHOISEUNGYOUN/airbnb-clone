bc6a2999eb4e73afe0f3d80c9fae881b512e2f9e
CHOISEUNGYOUN <alchemist718@gmail.com>
2020-03-19 14:53:09 +0900

    in progress of reservation detial

--- reservations/models.py
+++ reservations/models.py
@@ -2,7 +2,9 @@ import datetime
 
 from django.db    import models
 from django.utils import timezone
-from core         import models as core_models
+
+from core import models as core_models
+from .    import managers
 
 class BookedDay(core_models.TimeStampedModel):
 
@@ -35,6 +37,8 @@ class Reservation(core_models.TimeStampedModel):
     check_out = models.DateField()
     guest     = models.ForeignKey("users.User", on_delete=models.CASCADE)
     room      = models.ForeignKey("rooms.room", on_delete=models.CASCADE)
+    
+    objects = managers.CustomReservationManager()
 
     def __str__(self):
         return f'{self.room} - {self.check_in}'
